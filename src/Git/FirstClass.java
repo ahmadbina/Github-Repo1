7e93f3d0054f0ef4602d1016b4667105ff3aa6eb
ahmadbina <97071721+ahmadbina@users.noreply.github.com>
2022-01-07 11:18:21 -0500

    Update FirstClass.java
    BirdMethod

--- src/Git/FirstClass.java
+++ src/Git/FirstClass.java
@@ -1,14 +1,19 @@
 package Git;
 
-public class ScreenShotEX {
+public class Birds {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println(Bird.Egale("Egale\n sound:-frrrr", "\nFly:-high fly attiude 4000ft"));
+		System.out.println("---------------------------------------------------------------------");
+		System.out.println(Bird.Prrat("Prrat\n Walk:- walking Fast", "\nEat:-eat anything"));
 
 	}
-public static void takeScreenShot(String fileName) throws IOException {
-		String path =".\\screenshots";
-		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(file, new File(path+fileName+".png"));
 	}
+public static String Egale(String sound, String fly) {
+	return sound+fly;
+}
+public static String Prrat(String Walk, String eat) {
+
+	return Walk+eat;
+}
 }

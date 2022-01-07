f2ec44fe0cbec8ae6b65e71824b4abd8c1f20124
ahmadbina <97071721+ahmadbina@users.noreply.github.com>
2022-01-07 11:07:20 -0500

    Update FirstClass.java

--- src/Git/FirstClass.java
+++ src/Git/FirstClass.java
@@ -1,10 +1,14 @@
 package Git;
 
-public class FirstClass {
+public class ScreenShotEX {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 
 	}
-
+public static void takeScreenShot(String fileName) throws IOException {
+		String path =".\\screenshots";
+		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File(path+fileName+".png"));
+	}
 }

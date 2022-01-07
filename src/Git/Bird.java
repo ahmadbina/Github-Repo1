75614fce60d02958686b2e17aed7e76dcb75bdb5
ahmadbina <binaahmad616@gmail.com>
2022-01-07 11:45:31 -0500

    Bird Class

---
+++ src/Git/Bird.java
@@ -0,0 +1,21 @@
+package Git;
+
+public class Bird {
+
+	public static void main(String[] args) {
+		
+			System.out.println(Bird.Egale("Egale\n sound:-frrrr", "\nFly:-high fly attiude 4000ft"));
+			System.out.println("---------------------------------------------------------------------");
+			System.out.println(Bird.Prrat("Prrat\n Walk:- walking Fast", "\nEat:-eat anything"));
+		}
+	public static String Egale(String sound, String fly) {
+		return sound+fly;
+	}
+	public static String Prrat(String Walk, String eat) {
+
+		return Walk+eat;
+	
+
+	}
+
+}
